45382faf9ae947299f660473ec01a4d58368401b
Mattia Bondanza <mattia.bondanza@inventati.org>
2020-11-27 18:48:40 +0100

    Routine for mndo force and gradients added.

---
+++ source/emndoqm.f
@@ -0,0 +1,6 @@
+      subroutine emndoqm()
+        use mndo
+        
+        write(6, *) "DEBUG: EMNDOQM"
+        call mndomkin
+      end
---
+++ source/emndoqm1.f
@@ -0,0 +1,6 @@
+      subroutine emndoqm1()
+        use mndo
+        
+        write(6, *) "DEBUG: EMNDOQM1"
+        call mndomkin
+      end
