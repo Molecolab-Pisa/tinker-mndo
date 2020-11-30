561af0b0b52daae9f9f0d8009f3bada6060bc2ff
Mattia Bondanza <mattia.bondanza@inventati.org>
2020-11-30 17:47:55 +0100

    Added a forgotten file...

---
+++ source/mndolainit.f
@@ -0,0 +1,44 @@
+      subroutine mndolainit()
+        use atoms
+        use couple
+        use mndo
+
+        implicit none
+
+        integer :: i, j, k, iatqm
+
+c       Find out if a link atom is needed
+        mndo_usela = .false.
+        mndo_nla = 0
+        do i=1, nqmatoms
+          iatqm = qmlist(i)
+          do j=1, n12(iatqm)
+            do k=1, n-nqmatoms
+              if(i12(j,iatqm) .eq. mmlist(k)) then
+                mndo_usela = .true.
+                mndo_nla = mndo_nla + 1
+                mndo_laqm(mndo_nla) = iatqm
+                mndo_lamm(mndo_nla) = i12(j,iatqm)
+              end if
+            end do
+          end do
+        end do
+        
+        if(mndo_debug) then
+          write(6, *) ""
+          write(6, *) "Link atom table"
+          write(6, *) "|  QM  |  MM  |"
+          write(6, *) "---------------"
+          do i=1, mndo_nla
+            write(6, "(' | ', I4,' | ', I4,' |')") mndo_laqm(i),
+     &       mndo_lamm(i)
+          end do
+          write(6, *) "---------------"
+          write(6, *) ""
+        end if
+
+        if(mndo_usela) then
+          write(6, *) "Link atom are still not implemented."
+c          call fatal
+        end if
+      end  
